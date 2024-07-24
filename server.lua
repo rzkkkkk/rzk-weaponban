50bb22e88d0409f0ecf9f55cea25cac51b060ee9
Vasco Martins <104381804+rzkkkkk@users.noreply.github.com>
2024-07-25 00:40:20 +0200

    Update server.lua
    revert

--- server.lua
+++ server.lua
@@ -1,3 +1,5 @@
+-- server/weaponban.lua
+
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local function parseDuration(duration)
@@ -16,6 +18,16 @@ local function parseDuration(duration)
     return hours * 3600 + minutes * 60 + seconds
 end
 
+local function parseDate(dateString)
+    local pattern = "(%d+)-(%d+)-(%d+) (%d+):(%d+):(%d+)"
+    local year, month, day, hour, min, sec = dateString:match(pattern)
+    if year and month and day and hour and min and sec then
+        return os.time({year = year, month = month, day = day, hour = hour, min = min, sec = sec})
+    else
+        return nil
+    end
+end
+
 QBCore.Commands.Add('weaponban', 'Ban a player from using weapons', {{name='id', help='Player ID'}, {name='duration', help='Duration (e.g., 1h30m)'}}, true, function(source, args)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
@@ -28,9 +40,9 @@ QBCore.Commands.Add('weaponban', 'Ban a player from using weapons', {{name='id',
             local banDuration = parseDuration(duration)
             local banEnd = os.time() + banDuration
 
-            exports['oxmysql']:execute('UPDATE players SET weapon_ban_end = @weapon_ban_end WHERE citizenid = @citizenid', {
-                ['@weapon_ban_end'] = os.date('%Y-%m-%d %H:%M:%S', banEnd),
-                ['@citizenid'] = Target.PlayerData.citizenid
+            exports.oxmysql:execute('UPDATE players SET weapon_ban_end = ? WHERE citizenid = ?', {
+                os.date('%Y-%m-%d %H:%M:%S', banEnd),
+                Target.PlayerData.citizenid
             }, function()
                 TriggerClientEvent('QBCore:Notify', src, 'Player weapon banned successfully.', 'success')
                 TriggerClientEvent('QBCore:Notify', targetId, 'You have been banned from using weapons.', 'error')
@@ -52,8 +64,8 @@ QBCore.Commands.Add('weaponunban', 'Unban a player from using weapons', {{name='
 
     if QBCore.Functions.HasPermission(src, 'god') then
         if Target then
-            exports['oxmysql']:execute('UPDATE players SET weapon_ban_end = NULL WHERE citizenid = @citizenid', {
-                ['@citizenid'] = Target.PlayerData.citizenid
+            exports.oxmysql:execute('UPDATE players SET weapon_ban_end = NULL WHERE citizenid = ?', {
+                Target.PlayerData.citizenid
             }, function()
                 TriggerClientEvent('QBCore:Notify', src, 'Player weapon unbanned successfully.', 'success')
                 TriggerClientEvent('QBCore:Notify', targetId, 'You have been unbanned from using weapons.', 'success')
@@ -67,40 +79,19 @@ QBCore.Commands.Add('weaponunban', 'Unban a player from using weapons', {{name='
     end
 end, 'god')
 
-QBCore.Commands.Add('weaponbanstatus', 'Check your weapon ban status', {}, false, function(source, args)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-
-    exports['oxmysql']:execute('SELECT weapon_ban_end FROM players WHERE citizenid = @citizenid', {
-        ['@citizenid'] = Player.PlayerData.citizenid
-    }, function(result)
-        if result[1] and result[1].weapon_ban_end then
-            local banEndTime = os.time({year = tonumber(result[1].weapon_ban_end:sub(1, 4)), month = tonumber(result[1].weapon_ban_end:sub(6, 7)), day = tonumber(result[1].weapon_ban_end:sub(9, 10)), hour = tonumber(result[1].weapon_ban_end:sub(12, 13)), min = tonumber(result[1].weapon_ban_end:sub(15, 16)), sec = tonumber(result[1].weapon_ban_end:sub(18, 19))})
-            if banEndTime > os.time() then
-                local timeLeft = os.date('%X', os.difftime(banEndTime, os.time()))
-                TriggerClientEvent('QBCore:Notify', src, 'Your weapon ban ends in ' .. timeLeft, 'info')
-            else
-                TriggerClientEvent('QBCore:Notify', src, 'You are not currently banned from using weapons.', 'success')
-            end
-        else
-            TriggerClientEvent('QBCore:Notify', src, 'You are not currently banned from using weapons.', 'success')
-        end
-    end)
-end)
-
 AddEventHandler('QBCore:Server:PlayerLoaded', function(Player)
     local src = Player.PlayerData.source
 
-    exports['oxmysql']:execute('SELECT weapon_ban_end FROM players WHERE citizenid = @citizenid', {
-        ['@citizenid'] = Player.PlayerData.citizenid
+    exports.oxmysql:execute('SELECT weapon_ban_end FROM players WHERE citizenid = ?', {
+        Player.PlayerData.citizenid
     }, function(result)
         if result[1] and result[1].weapon_ban_end then
-            local banEndTime = os.time({year = tonumber(result[1].weapon_ban_end:sub(1, 4)), month = tonumber(result[1].weapon_ban_end:sub(6, 7)), day = tonumber(result[1].weapon_ban_end:sub(9, 10)), hour = tonumber(result[1].weapon_ban_end:sub(12, 13)), min = tonumber(result[1].weapon_ban_end:sub(15, 16)), sec = tonumber(result[1].weapon_ban_end:sub(18, 19))})
-            if banEndTime > os.time() then
+            local banEndTime = parseDate(result[1].weapon_ban_end)
+            if banEndTime and banEndTime > os.time() then
                 TriggerClientEvent('QBCore:Client:CheckWeaponBan', src, true)
             else
-                exports['oxmysql']:execute('UPDATE players SET weapon_ban_end = NULL WHERE citizenid = @citizenid', {
-                    ['@citizenid'] = Player.PlayerData.citizenid
+                exports.oxmysql:execute('UPDATE players SET weapon_ban_end = NULL WHERE citizenid = ?', {
+                    Player.PlayerData.citizenid
                 })
             end
         end
